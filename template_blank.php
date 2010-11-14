604b244ad6acce1e95733fa4028641a0f82c5d89
PJ Onori <pj@somerandomdude.com>
2010-11-13 22:56:34 -0800

    Added a blank Wordpress page template for additional AJAX calls.

---
+++ template_blank.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Template Name: Franklin Street Blank Template
+
+http://codex.wordpress.org/Function_Reference/wp_insert_post
+http://codex.wordpress.org/Function_Reference/update_post_meta
+http://codex.wordpress.org/Function_Reference/post_meta_Function_Examples
+http://hungred.com/how-to/wordpress-templatepage-meta-key-updatepostmeta/
+http://www.emanueleferonato.com/2010/04/01/loading-wordpress-posts-with-ajax-and-jquery/
+
+*/
+?>
+<?php if(have_posts()) : while(have_posts()) : the_post(); ?><?php the_content(); ?><?php endwhile; endif; ?>
