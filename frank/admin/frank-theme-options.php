recprog/Frank

commit 061a14ff94c9bf525c162c05adade989e7178ff2

Disable Section Title + Header if box unchecked.

frank/admin/frank-theme-options.php
@@ -331,13 +331,15 @@ function frank_build_settings_page() {
 
 							<div class="top-options-container">
 	
-								<?php $the_type = $frank_section['display_type']; ?>
-								
+								<?php
+								$the_type = $frank_section['display_type'];
+								$checkbox_name = "frank-section-header-" . isset($frank_section['default']) ? 'default' : $frank_section_id;
+								?>
 
 								<!-- // SECTION HEADER TOGGLE -->
 								<div class="display-headers">
 									<input type="checkbox"
-								   name="frank-section-header-<?php echo (isset($frank_section['default']) ? 'default' : $frank_section_id); ?>"
+								   name=$checkbox_name
 									class="checkbox"
 									value="section_header"
 									<?php 
@@ -349,7 +351,7 @@ function frank_build_settings_page() {
 								</div>
 
 								<!-- // SECTION TITLE -->
-								<div class="display-titles">
+								<div class="display-titles optional-container" controlling-checkbox=$checkbox_name>
 									<label class="section-title"><?php _e('Section Title:', 'frank'); ?></label>
 									<input type="text"
 										   class="text text-title"
@@ -387,7 +389,7 @@ function frank_build_settings_page() {
 							<div class="bottom-options-container">
 								
 								<!-- // SECTION CAPTIONS -->
-								<div class="display-captions">
+								<div class="display-captions optional-container" controlling-checkbox=$checkbox_name>
 									<label class="section-title"><?php _e('Section Caption:', 'frank'); ?></label>
 									<textarea name="frank-section-caption-<?php echo (isset($frank_section['default']) ? 'default' : $frank_section_id); ?>" class="textarea"><?php echo !isset($frank_section['default']) ? stripslashes($frank_section['caption']) : $frank_defaults['caption']; ?></textarea>
 								</div>

commit d3ff94a154f225bdeebabbd6cb8687fefdd2e040

[FIX] bugs introduced in 061a14ff94 ("Disable…")
  * echo content of $checkbox_name
  * fix precedence logic to generate checkbox_name

frank/admin/frank-theme-options.php
@@ -333,13 +333,13 @@ function frank_build_settings_page() {
 	
 								<?php
 								$the_type = $frank_section['display_type'];
-								$checkbox_name = "frank-section-header-" . isset($frank_section['default']) ? 'default' : $frank_section_id;
+								$checkbox_name = "frank-section-header-" . (isset($frank_section['default']) ? 'default' : $frank_section_id);
 								?>
 
 								<!-- // SECTION HEADER TOGGLE -->
 								<div class="display-headers">
 									<input type="checkbox"
-								   name=$checkbox_name
+								   name=<?php echo $checkbox_name ?>
 									class="checkbox"
 									value="section_header"
 									<?php 
@@ -351,7 +351,7 @@ function frank_build_settings_page() {
 								</div>
 
 								<!-- // SECTION TITLE -->
-								<div class="display-titles optional-container" controlling-checkbox=$checkbox_name>
+								<div class="display-titles optional-container" controlling-checkbox=<?php echo $checkbox_name ?> >
 									<label class="section-title"><?php _e('Section Title:', 'frank'); ?></label>
 									<input type="text"
 										   class="text text-title"
@@ -397,7 +397,7 @@ function frank_build_settings_page() {
 							<div class="bottom-options-container">
 								
 								<!-- // SECTION CAPTIONS -->
-								<div class="display-captions optional-container" controlling-checkbox=$checkbox_name>
+								<div class="display-captions optional-container" controlling-checkbox=<?php echo $checkbox_name ?> >
 									<label class="section-title"><?php _e('Section Caption:', 'frank'); ?></label>
 									<textarea name="frank-section-caption-<?php echo (isset($frank_section['default']) ? 'default' : $frank_section_id); ?>"
 									  <?php