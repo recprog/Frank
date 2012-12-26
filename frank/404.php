c8bafd714bf207d09ed8be9b01ff36e853801b5a
Felix Holmgren <felix@sunnyzebra.com>
2012-12-26 16:51:53 +0100

    404 localization.

--- frank/404.php
+++ frank/404.php
@@ -7,15 +7,29 @@
 <div id="content" class="fullspread fourohfour">
 	<div id="content-primary">
 		<header>
-			<h1>Page Not Found</h1>
+			<h1>
+			  <?php _e('Page Not Found', 'frank_theme'); ?>
+			</h1>
 		</header>
 		<div class='container'>
 			<div class='row'>
 				<div class="six columns">
-					<p class='large'>Unfortunately, the page you are looking for no longer exists or never existed in the first place. If you reached this page in error, you can go <a href="<?php echo home_url(); ?>" title="<?php bloginfo('name'); ?>">home</a> and start over.</p>
+					<p class='large'>
+					<?php
+					  $home_link = sprintf('<a href="%s" title="%s">%s</a>',
+					                      home_url(),
+					                      get_bloginfo('name'),
+					                      _x('home', 'home_link_text', 'frank_theme'));
+					  echo sprintf(__('Unfortunately, the page you are looking for no longer exists or never existed in the first place. If you reached this page in error, you can go %s and start over.', 'frank_theme'), $home_link);
+					?>
+					</p>
 				</div>
 				<div class="six columns search">
-					<p class='large'>If you believe this page exists, please try searching for the page in the search input below.</p>
+					<p class='large'>
+					<?php
+					  _e('If you believe this page exists, please try searching for the page in the search input below.', 'frank_theme');
+					?>
+					</p>
 					<?php get_search_form(); ?>
 				</div>
 			</div>
