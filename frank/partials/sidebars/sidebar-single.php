e0798b538a42292d7a7f61f404c0c3b097ef8315
Felix Holmgren <felix@sunnyzebra.com>
2012-12-26 01:20:50 +0100

    Search + "About this site" localization.
      * TODO: Lots of duplication between these files.

--- frank/partials/sidebars/sidebar-single.php
+++ frank/partials/sidebars/sidebar-single.php
@@ -5,9 +5,13 @@
 ?>
 	<aside id="sidebar" role="complementary">	
 			<?php if ( !dynamic_sidebar('Post Right Aside') ) : ?>
-				<h3>About This Site</h3>
+				<h3>
+				  <?php _e('About This Site', 'frank_theme'); ?>
+				</h3>
 				<p><?php bloginfo('description'); ?></p>
-				<h3>Search</h3>
+				<h3>
+				  <?php _e('Search', 'frank_theme'); ?>
+				</h3>
 				<?php get_search_form(); ?>
 			<?php endif; ?>
 	</aside>
