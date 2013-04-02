15b8e3e030a7f884d3f0ca7ce5ea4b7e61b33cc0
P.J. Onori <pj@somerandomdude.com>
2013-04-02 11:20:58 -0700

    Added Coffeescripts and Javascripts to Frank theme.
    Duplicate files were left in the Somerandomdude theme for now. These
    will be removed once build automation is added in 0.9.3

--- somerandomdude/header.php
+++ somerandomdude/header.php
@@ -30,7 +30,7 @@
 	<!--[if lt IE 7]>
 	<script src="http://ie7-js.googlecode.com/svn/version/2.1(beta4)/IE7.js"></script>
 	<![endif]-->
-	
+
 </head>
 <body id="page" <?php body_class(); ?>>
 <div class='container'>
