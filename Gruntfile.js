e028fb2c70842103737de3776e16b4b59a92bed0
Rob Dodson <lets.email.rob@gmail.com>
2013-05-25 08:31:52 -0700

    Add matchdep to clean up long list of `loadNpmTasks` calls

--- Gruntfile.js
+++ Gruntfile.js
@@ -224,25 +224,13 @@ module.exports = function(grunt) {
         }
     });
 
+    // load all grunt tasks
+    require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
     grunt.loadNpmTasks('svgo-grunt');
-    grunt.loadNpmTasks('grunt-contrib-clean');
-    grunt.loadNpmTasks('grunt-contrib-coffee');
-    grunt.loadNpmTasks('grunt-contrib-compress');
-    grunt.loadNpmTasks('grunt-contrib-concat');
-    grunt.loadNpmTasks('grunt-contrib-copy');
-    grunt.loadNpmTasks('grunt-contrib-csslint');
-    grunt.loadNpmTasks('grunt-contrib-imagemin');
-    grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-contrib-sass');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-csso');
-    grunt.loadNpmTasks('grunt-csscss');
-    grunt.loadNpmTasks('grunt-git-contributors');
-    grunt.loadNpmTasks('grunt-markdown');
-    grunt.loadNpmTasks('grunt-phpcs');
-    grunt.loadNpmTasks('grunt-webp');
 
+    /**
+     * Grunt tasks for development.
+     */
     grunt.registerTask('default', ['coffee', 'concat', 'sass:dev', 'watch']);
 
     /**
